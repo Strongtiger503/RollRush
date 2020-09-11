27f67093593b6f48ad9fb002020afc8570eb7130
Strongtiger503 <Mero200524@gmail.com>
2020-09-11 16:20:07 +0200

    Few Tweaks

---
+++ Roll Rush/Assets/Game Assets/Scripts/Restart/Restartrestartnumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restartrestartnumber : MonoBehaviour
+{
+    
+    #region Main
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        RestartFunctions.RestartNumber = 0;
+
+    }
+
+    #endregion
+
+}
